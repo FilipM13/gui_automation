cd7584a4dfc508eb727d3239dfe1ca83d7389524
Filip Matejko <matejkofilip@gmail.com>
2024-04-16 12:56:22 +0200

    Initial process class structure.

--- src/elements/process.py
+++ src/elements/process.py
@@ -7,3 +7,36 @@ Single process created from a function. Consists of:
 - execution button
 - message window
 """
+
+
+class Process:
+
+    @classmethod
+    def from_function(function, name=None, version=None, description=None):
+        slf = Process(
+            function=function, name=name, version=version, description=description
+        )
+        slf.create_inputs()
+        slf.create_logger
+        return slf
+
+    @classmethod
+    def from_module(module):
+        f = None
+        n = None
+        v = None
+        d = None
+        slf = Process(function=f, name=n, version=v, description=d)
+        slf.create_inputs()
+        slf.create_logger
+        return slf
+
+    def __init__(self, function, name=None, version=None, description=None) -> None:
+
+        pass
+
+    def create_inputs(self):
+        pass
+
+    def create_logger(self):
+        pass
